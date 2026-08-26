86656813d421e2cc94252260fca19085f03f8be6
miachenying <miachenying@gmail.com>
2026-08-26 10:23:57 -0700

    Remove unused data integrity helper

--- lib/vocabulary/data-integrity.ts
+++
@@ -1,3 +0,0 @@
-export function sameVocabularyIdentity(leftUserId: string, leftCanonical: string, rightUserId: string, rightCanonical: string) {
-  return leftUserId === rightUserId && leftCanonical === rightCanonical;
-}
